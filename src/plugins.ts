bdbf1ab0303789e4f53658309ce25d852ce61cbe
Benedikt Werner <1benediktwerner@gmail.com>
2024-11-03 16:29:30 +0100

    Log to new channel

--- src/plugins.ts
+++ src/plugins.ts
@@ -5,7 +5,7 @@ import nameUpdater from './plugins/name-updater.js';
 import { default as points } from './plugins/points.js';
 
 const CHANNEL_DISCORD_GAMES_TRIVIA = '493061298486116352';
-const CHANNEL_NEW_PEEPS = '508080534648258563';
+const CHANNEL_LOG = '1302655272707166330';
 const SECONDS_IN_HOUR = 60 * 60;
 
 const ROLE_IMAGES = '477557270943760386';
@@ -28,6 +28,6 @@ export default [
         adminRoles: [ROLE_CHAT_MOD, ROLE_ADMIN],
     }),
     messagesLog({
-        logChannel: CHANNEL_NEW_PEEPS,
+        logChannel: CHANNEL_LOG,
     }),
 ];
