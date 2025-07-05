4f7bf624a17d1e3940bbdd1b74b70cfad4a90720
Benedikt Werner <1benediktwerner@gmail.com>
2025-07-05 16:54:24 +0200

    Fix migration 3

--- migrations/3.sql
+++ migrations/3.sql
@@ -1,5 +1,11 @@
 ALTER TABLE users
-    DROP points,
-    DROP lastPointsTimestamp,
-    ADD firstActivityTimestamp INT NOT NULL DEFAULT 0,
+    DROP points;
+
+ALTER TABLE users
+    DROP lastPointsTimestamp;
+
+ALTER TABLE users
+    ADD firstActivityTimestamp INT NOT NULL DEFAULT 0;
+
+ALTER TABLE users
     ADD messageCount INT NOT NULL DEFAULT 0;
